061c948cdd7af0229ed02d653d89f1162a0b5064
Rohit <therohitsingh82@gmail.com>
2023-03-23 12:07:57 +0530

    shop logic complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import { Shop } from "./pages/shop/Shop";
+import { Cart } from "./pages/cart/Cart";
+import { ShopContextProvider } from "./context/shop-context";
 function App() {
   return (
     <div className="App">
-     Rohit
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
---
+++ src/pages/cart/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Cart = () => {
+  return (
+    <div>
+      lorem20000
+    </div>
+  )
+}
+
+
